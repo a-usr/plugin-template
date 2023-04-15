1133ea7e24714932b2d3707aa71d50b031a7295f
usr <81042605+a-usr@users.noreply.github.com>
2023-04-15 10:01:40 +0000

    settings

--- plugins/test/src/Settings.tsx
+++ plugins/test/src/Settings.tsx
@@ -1,8 +1,23 @@
-import { Forms } from "@vendetta/ui/components";
+import { Forms, General, Alert, Button, HelpMessage, SafeAreaView } from "@vendetta/ui/components";
 const { FormText } = Forms;
 
 export default () => (
     <FormText>
         Hello, world!
     </FormText>
+    <General>
+        General
+    </General>
+    <Alert>
+        Alert
+    </Alert>
+    <Button>
+        Button
+    </Button>
+    <HelpMessage>
+        Helpmsg
+    </HelpMessage>
+    <SafeAreaView>
+        SafeAreaView
+    </SafeAreaView>
 )
