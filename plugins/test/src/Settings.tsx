e70bc1b4a15a0fc7fa12fdce77088ebc964ac0f3
usr <81042605+a-usr@users.noreply.github.com>
2023-04-15 10:15:56 +0000

    do

--- plugins/test/src/Settings.tsx
+++ plugins/test/src/Settings.tsx
@@ -2,22 +2,9 @@ import { Forms, General, Alert, Button, HelpMessage, SafeAreaView } from "@vende
 const { FormText } = Forms;
 
 export default () => (
-    <FormText>
-        Hello, world!
-        <General>
-            General
-        </General>
-        <Alert>
-            Alert
-        </Alert>
-        <Button>
-            Button
-        </Button>
-        <HelpMessage>
-            Helpmsg
-        </HelpMessage>
-        <SafeAreaView>
-            SafeAreaView
-        </SafeAreaView>
-    </FormText>
+    <General>
+        <FormText>
+            Hello World
+        </FormText>
+    </General>
 )
